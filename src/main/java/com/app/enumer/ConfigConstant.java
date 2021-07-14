c5bfdf8e12c549a1f493de261cebda57fc127b0a
Raynanda Aufa <raynandaufa@gmail.com>
2021-07-14 13:13:42 +0700

    update prod

--- src/main/java/com/app/enumer/ConfigConstant.java
+++ src/main/java/com/app/enumer/ConfigConstant.java
@@ -2,11 +2,18 @@ package com.app.enumer;
 
 public enum ConfigConstant {
 	
+//	accountNumber_Interbank("0316031099"),
+//	secretKey("ab80ff61-bb0e-4f36-8cb1-e5d80796ace9"),
+//	xApiKey("94eb95e2-f38b-4fe2-bbc5-6cad256fccc3"),
+//	clientId("SANDBOX"),
+//	usernamePassword("186703ca-141a-496b-b43c-396465f99324:ed14bf2e-e6ba-4ffb-89f6-6124392b8045");
+	
+	//PRODUCTION
 	accountNumber_Interbank("0316031099"),
-	secretKey("ab80ff61-bb0e-4f36-8cb1-e5d80796ace9"),
-	xApiKey("94eb95e2-f38b-4fe2-bbc5-6cad256fccc3"),
-	clientId("SANDBOX"),
-	usernamePassword("186703ca-141a-496b-b43c-396465f99324:ed14bf2e-e6ba-4ffb-89f6-6124392b8045");
+	secretKey("8995c547-684b-4cf7-9dff-702170f00d8f"),
+	xApiKey("9212c17f-4b08-4324-9e31-bebe5273a28b"),
+	clientId("MSIG"),
+	usernamePassword("bf7a10aa-2110-45ac-a03e-0f904ac53dc9:dce09f31-25e7-4a70-8f74-7e384537fd81");
 	
 	private String value;
 
--- src/main/java/com/app/enumer/Environment.java
+++ src/main/java/com/app/enumer/Environment.java
@@ -2,9 +2,14 @@ package com.app.enumer;
 
 public enum Environment {
 	
-	GET_TOKEN("https://apidev.bni.co.id:8067/api/oauth/token"),
-	GET_INHOUSE_INQUIRY("https://apidev.bni.co.id:8067/H2H/v2/getinhouseinquiry"),
-	GET_INTERBANK_INQUIRY("https://apidev.bni.co.id:8067/H2H/v2/getinterbankinquiry");
+//	GET_TOKEN("https://apidev.bni.co.id:8067/api/oauth/token"),
+//	GET_INHOUSE_INQUIRY("https://apidev.bni.co.id:8067/H2H/v2/getinhouseinquiry"),
+//	GET_INTERBANK_INQUIRY("https://apidev.bni.co.id:8067/H2H/v2/getinterbankinquiry");
+	
+	//PRODUCTION
+	GET_TOKEN("https://api.bni.co.id:443/api/oauth/token"),
+	GET_INHOUSE_INQUIRY("https://api.bni.co.id:443/H2H/v2/getinhouseinquiry"),
+	GET_INTERBANK_INQUIRY("https://api.bni.co.id:443/H2H/v2/getinterbankinquiry");
 	
 	private String url;
 
